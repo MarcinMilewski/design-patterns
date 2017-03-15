b11d02c5dea9783a1d6dfcd5d7acd17127772049
marcin <marcin.milewski@mail.com>
2017-03-15 23:28:50 +0100

    Singleton

---
+++ src/test/java/SingletonSpec.groovy
@@ -0,0 +1,15 @@
+import patterns.Singleton
+import spock.lang.Specification;
+
+class SingletonTest extends Specification {
+    def "Should be only one instance"() {
+        given:
+        Singleton instance1 = Singleton.getInstance()
+        Singleton instance2 = Singleton.getInstance()
+
+        expect:
+        instance1.is(instance2)
+
+
+    }
+}
