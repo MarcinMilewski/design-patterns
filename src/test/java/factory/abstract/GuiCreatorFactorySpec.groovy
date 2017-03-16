4dc6d374de431c0b299848b12123e3eb58ea0920
marcin <marcin.milewski@mail.com>
2017-03-16 11:10:19 +0100

    gui creator spec

---
+++ src/test/java/factory/abstract/GuiCreatorFactorySpec.groovy
@@ -0,0 +1,11 @@
+import spock.lang.Specification
+
+class GuiCreatorFactorySpec extends Specification {
+    def "Should create minimal gui"() {
+
+    }
+
+    def "Should create complete gui"() {
+
+    }
+}
